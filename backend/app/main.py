9d94ad07e93fd0679474383bd2221dc1ec4bd211
MarioMatheus <mariomatheu@gmail.com>
2019-12-01 16:23:56 -0300

    config compose file, add cors to server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, HTTPException, Depends
 from .service import Service, ServiceUnavailable
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
--- frontend/src/config/service.js
+++ frontend/src/config/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const KryptocoinAPI = axios.create({
-  baseURL: process.env['API_URL'] || 'https://localhost:3001',
+  baseURL: 'https://localhost:3001',
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
